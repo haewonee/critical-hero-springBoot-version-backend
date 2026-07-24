8885921bfd59a53e7fb31800d55acca2e9ed14d7
haewonee <godnjs5870@naver.com>
2026-07-24 21:34:06 +0900

    Initial commit: 초기 설정

---
+++ src/main/java/com/devassistant/critical_hero_springboot_version/CriticalHeroSpringbootVersionApplication.java
@@ -0,0 +1,13 @@
+package com.devassistant.critical_hero_springboot_version;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriticalHeroSpringbootVersionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriticalHeroSpringbootVersionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devassistant/critical_hero_springboot_version/CriticalHeroSpringbootVersionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devassistant.critical_hero_springboot_version;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriticalHeroSpringbootVersionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
